6bc0d3be588391a056f77697f7ae804fc4f08832
Dawood <dreamboy.dani@gmail.com>
2020-05-14 01:30:58 +0000

    Updating the Array loop file

--- packing-list.js
+++ packing-list.js
@@ -1,7 +1,15 @@
-// Looping through Arrays
+// Looping through Arrays using For loop
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+// Looping through Array using  While loop.
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
