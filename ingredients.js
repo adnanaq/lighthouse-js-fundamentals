76c6da7f8c95db0c1947a206b941f2f25d1dd442
Dawood <dreamboy.dani@gmail.com>
2020-05-14 01:53:44 +0000

    Adding ingredient js file solution

---
+++ ingredients.js
@@ -0,0 +1,21 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+// Write a for loop that prints out the contents of ingredients:
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+// Write any loop(while or for) that prints out the contents of ingredients backwards:
+let revArr = ingredients.reverse();
+let j = 0;
+while (j < ingredients.length) {
+  console.log(ingredients[j]);
+  j++;
+}
